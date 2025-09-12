c3fffda2dfa27148098b889c594453935cdd38d6
Luka Vlatkovic <vlatkovicluka01@gmail.com>
2025-09-12 19:01:53 +0200

    Refactored some of 2,3 task and finished Task 4.

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,34 +3,69 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-System.Console.WriteLine("Unesite svoje korisnicko ime:");
-string? korisnik = Console.ReadLine();
-
-
-
-
-//kreiranje uticnice za slanje podataka
-Socket sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-//Podesavanje adrese primaoca
-IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27015);
-
-//Poruka za slanje
-string meesage = $"MENADZER:[{korisnik}]";
-byte[] messageBytes = Encoding.UTF8.GetBytes(meesage);
-
-try
+class Manadzer
 {
-    int bytesSent = sendSocket.SendTo(messageBytes, 0, messageBytes.Length, SocketFlags.None, recvEndPoint);
-    System.Console.WriteLine("Sent {0} bytes to {1}", bytesSent, recvEndPoint);
-}
-catch (System.Exception ex)
-{
-    System.Console.WriteLine("sendto failed with error: {0}", ex.Message);
-    throw;
-}
-finally
-{
-    //zatvaranje uticnice nakon slanja
-    sendSocket.Close();
+    static void Main()
+    {
+        string filePath = "menadzer.txt";
+        string korisnickoIme;
+
+        if (File.Exists(filePath))
+        {
+            korisnickoIme = File.ReadAllText(filePath);
+            System.Console.WriteLine($"Dobrodosli nazad: {korisnickoIme}!");
+        }
+        else
+        {
+            System.Console.WriteLine("Unesite korisnicko ime:");
+            korisnickoIme = Console.ReadLine() ?? string.Empty;
+            File.WriteAllText(filePath, korisnickoIme);
+        }
+
+        int udpPort = 9000;
+        //Kreiranje udp socketa za slanje poruke
+        Socket udpClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //Podesavanje adrese primaoca
+        IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, udpPort);
+
+        //Kreiranje praznog baffera i priprema EP da u njega upisemo Adresu Servera
+        byte[] buffer = new byte[1024];
+        EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+        string message = $"MENADZER:{korisnickoIme}";
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        System.Console.WriteLine(message);
+        udpClientSocket.SendTo(messageBytes, serverEP);
+
+        //Prijem poruke sa servera
+        int received = udpClientSocket.ReceiveFrom(buffer, ref remoteEP);
+        string response = Encoding.UTF8.GetString(buffer, 0, received);
+        System.Console.WriteLine($"Server je odgovorio sa porukom: {response}");
+
+        if (response.StartsWith("TCP_PORT:"))
+        {
+            int tcpPort = int.Parse(response.Split(':')[1]);
+            System.Console.WriteLine($"TCP Konekcija ce ici na portu: {tcpPort}.");
+            //Kreiranje tcp uticnice za povezivanje
+            Socket tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpClient.Connect(IPAddress.Loopback, tcpPort);
+
+            //Kreiranje zadatka
+            System.Console.WriteLine("Naziv zadatka");
+            string naziv = Console.ReadLine() ?? string.Empty;
+            System.Console.WriteLine("Zaposleni");
+            string zaposleni = Console.ReadLine() ?? string.Empty;
+            System.Console.WriteLine("Rok yyyy-mm-dd");
+            string rok = Console.ReadLine() ?? string.Empty;
+            System.Console.WriteLine("Prioritet (int)");
+            string prioritet = Console.ReadLine() ?? string.Empty;
+
+            string zadatakPoruka = $"{naziv}|{zaposleni}|{rok}|{prioritet}";
+            tcpClient.Send(Encoding.UTF8.GetBytes(zadatakPoruka));
+
+            System.Console.WriteLine("Zadatak poslat serveru");
+            tcpClient.Close();
+        }
+        udpClientSocket.Close();
+    }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -3,34 +3,95 @@ using System.Net.Sockets;
 using System.Text;
 using Server;
 
-//Dictionary string Korisnicko ime - Menadzer projekta i listom objekata klase ZadatakProjekta
-Dictionary<string, ZadatakProjekta> menadgerAndProjects = new Dictionary<string, ZadatakProjekta>();
-//Kreiranje uticnice za prijem datagrama
-Socket recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-//Povezivanje uticnice sa bilo kojom adresom na lokalnom racunaru i portom 27015
-IPEndPoint recvEndPoint = new IPEndPoint(IPAddress.Any, 27015);
-recvSocket.Bind(recvEndPoint);
 
-//Bafer za prijem podataka
-byte[] recvBuffer = new byte[1024];
-EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
-
-try
+class KolaborativniServis
 {
-    //Prijem datagrama
-    int bytesReceived = recvSocket.ReceiveFrom(recvBuffer, ref senderEndPoint);
+    static Dictionary<string, List<ZadatakProjekta>> zadaci = new Dictionary<string, List<ZadatakProjekta>>();
 
-    //Konverzija primljenih bajtova u string
-    string receivedMessage = Encoding.UTF8.GetString(recvBuffer, 0, bytesReceived);
-    System.Console.WriteLine("Received {0} bytes from {1}: {2}", bytesReceived, senderEndPoint, receivedMessage);
+    static void Main()
+    {
+        int udpPort = 9000;
+        int tcpPort = 10000;
+
+        //Kreiranje udp Socketa
+        Socket udpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        //Povezivanje uticnice sa bilo kojom adresom na lokalnom racunaru i portom 27015
+        IPEndPoint localUdpEndPoint = new IPEndPoint(IPAddress.Any, udpPort);
+        udpServer.Bind(localUdpEndPoint);
+        System.Console.WriteLine($"UDP Server pokrenut na portu {udpPort}...");
+
+        // TCP socket za prijem zadataka
+        Socket tcpListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint localTcpEndPoint = new IPEndPoint(IPAddress.Any, tcpPort);
+        tcpListener.Bind(localTcpEndPoint);
+        tcpListener.Listen(5);
+        System.Console.WriteLine($"TCP Server pokrenut na portu {tcpPort}");
+
+        //bafer za prijem podataka
+        byte[] recvBuffer = new byte[1024];
+        EndPoint remoteUdpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        try
+        {
+            while (true)
+            {
+                int received = udpServer.ReceiveFrom(recvBuffer, ref remoteUdpEndPoint);
+                string message = Encoding.UTF8.GetString(recvBuffer, 0, received);
+
+                System.Console.WriteLine($"Primljena poruka: {message}");
+
+                if (message.StartsWith("MENADZER:"))
+                {
+                    string korisnickoIme = message.Split(':')[1];
+                    if (!zadaci.ContainsKey(korisnickoIme))
+                    {
+                        zadaci[korisnickoIme] = new List<ZadatakProjekta>();
+                    }
+
+                    string odgovor = $"TCP_PORT:{tcpPort}";
+                    byte[] odgovorBytes = Encoding.UTF8.GetBytes(odgovor);
+                    udpServer.SendTo(odgovorBytes, remoteUdpEndPoint);
+                    System.Console.WriteLine($"Poslat TCP PORT Korisniku {korisnickoIme}");
+
+                    //Server dobija klijentsku uticnicu
+                    Socket client = tcpListener.Accept();
+                    System.Console.WriteLine("Menadzer povezan preko TCP!");
+
+                    //Primanje zadataka
+                    received = client.Receive(recvBuffer);
+                    string zadatakPoruka = Encoding.UTF8.GetString(recvBuffer, 0, received);
+                    System.Console.WriteLine($"Primljeno preko TCP: {zadatakPoruka}");
+
+                    //Parsiranje poruke i dodavanje u Dictionary
+                    string[] delovi = zadatakPoruka.Split('|');
+                    if (delovi.Length >= 4)
+                    {
+                        ZadatakProjekta zadatak = new ZadatakProjekta
+                        {
+                            Naziv = delovi[0],
+                            Zaposleni = delovi[1],
+                            Rok = DateTime.Parse(delovi[2]),
+                            Prioritet = int.Parse(delovi[3])
+                        };
+                        zadaci[korisnickoIme].Add(zadatak);
+                        System.Console.WriteLine($"Zadatak dodat: {zadatak.Naziv} za {zadatak.Zaposleni}");
+                    }
+                    client.Close();
+                }
+                else if (message.StartsWith("PREGLED:"))
+                {
+                    //Menadzer trazi pregled zadataka
+
+
+                }
+            }
+        }
+        finally
+        {
+            udpServer.Close();
+            tcpListener.Close();
+        }
+    }
 }
-catch (SocketException ex)
-{
-    System.Console.WriteLine("recvfrom failed with error {0}", ex.Message);
-    throw;
-}
-finally
-{
-    recvSocket.Close();
-}
+
+
